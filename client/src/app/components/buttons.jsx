d2ff48a1173975520e294df2c56aaac71e124a19
lucky verma <aadyant001001@gmail.com>
2024-02-25 10:26:26 +0530

    last after evalution

--- client/src/app/components/buttons.jsx
+++ client/src/app/components/buttons.jsx
@@ -1,14 +1,17 @@
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { useEffect } from "react";
+import Link from "next/link";
 
 import React, { useRef } from "react";
 
 function Buttons({ name1, name2 }) {
   return (
     <div className="start flex justify-center gap-4 py-8 my-2 lg:gap-16 lg:py-821">
+      <Link href='/room'>
       <button id="plans-button" className="button w-40 lg:w-44 text-lg crimson bg-blue-voilet" >
         {name1}
       </button>
+      </Link>
       <a href="https://calendly.com/arsh0/15min" target="_blank" rel="noopener noreferrer" className="button w-40 lg:w-44  text-lg  crimson bg-black flex justify-center">
         {name2} <FaExternalLinkAlt className='mt-1 ml-1'/>
       </a>
