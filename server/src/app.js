ea3d740dac8532226daf8afcc5c955de5e2f8a4b
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2024-02-25 05:47:23 +0530

    ai model integrated

--- server/src/app.js
+++ server/src/app.js
@@ -1,41 +1,70 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { spawn } from "child_process";
+import { ApiResponse } from "./utils/ApiResponse.js";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
+app.post("/calculate-bert-score", (req, res) => {
+  const data = req.body;
+  console.log(req.body);
+
+  
+  // Spawn a Python subprocess
+  const pythonProcess = spawn("python", [
+    "bert.py",
+    data.question,
+    data.user_answer,
+    JSON.stringify(data.correct_answers)
+    
+  ]); //,
+console.log(JSON.stringify(data.correct_answers));
+  
+
+  // Collect the output from the Python subprocess
+  let result ="";
+  pythonProcess.stdout.on("data", (data) => {
+    console.log("data", data.toString());
+    result += data;
+  });
+
+  pythonProcess.stderr.on("data", (error) => {
+    console.log("error", error.toString());
+  });
+
+  console.log("result", result);
+  // When the subprocess exits, send the result back in the response
+  pythonProcess.on("close", (code) => {
+    //const score = JSON.parse(result)?.score;
+    //res.json({ 'score': score });
+    console.log("process exited with code " + code);
+    return res
+    .status(201)
+    .json(new ApiResponse(201, result, "bert score"));
+  });
+
+  
+});
 
 //routes import
-import userRouter from './routes/user.routes.js'
-import healthcheckRouter from "./routes/healthcheck.routes.js"
-import tweetRouter from "./routes/tweet.routes.js"
-import subscriptionRouter from "./routes/subscription.routes.js"
-import videoRouter from "./routes/video.routes.js"
-import commentRouter from "./routes/comment.routes.js"
-import likeRouter from "./routes/like.routes.js"
-import playlistRouter from "./routes/playlist.routes.js"
-import dashboardRouter from "./routes/dashboard.routes.js"
+//import userRouter from './routes/user.routes.js'
 
 //routes declaration
-app.use("/api/v1/healthcheck", healthcheckRouter)
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/tweets", tweetRouter)
-app.use("/api/v1/subscriptions", subscriptionRouter)
-app.use("/api/v1/videos", videoRouter)
-app.use("/api/v1/comments", commentRouter)
-app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/playlist", playlistRouter)
-app.use("/api/v1/dashboard", dashboardRouter)
+
+//app.use("/api/v1/users", userRouter)
 
 // http://localhost:8000/api/v1/users/register
 
